c1ac3ed473a0cdc8362052ffcaf2c6418b8f944c
JauntyJJS <jauntyjjs@hotmail.com>
2019-05-10 00:18:24 +0800

    Try another version of mvtnorm

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-packageurl <- "https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-9.tar.gz"
+packageurl <- "https://cran.r-project.org/src/contrib/Archive/mvtnorm/mvtnorm_1.0-8.tar.gz"
 install.packages(packageurl, repos=NULL, type="source")
 install.packages("mclust")
 install.packages("flexmix")
